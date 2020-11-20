d47047de3d3e0b1fd9b28b173c377643980f9e11
Admin <admin@Santhoshs-MacBook-Pro.local>
2020-11-20 13:56:57 +0530

    Initial Commit

---
+++ SwiftUI-NetFlixClone/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI-NetFlixClone
+//
+//  Created by narendra. vadde on 20/11/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI-NetFlixClone/SwiftUI_NetFlixCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_NetFlixCloneApp.swift
+//  SwiftUI-NetFlixClone
+//
+//  Created by narendra. vadde on 20/11/20.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_NetFlixCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
